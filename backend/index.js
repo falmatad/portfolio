c7408a2b3c46bfc955b1f75ee2aaea8303c677cc
Falmata Dawano <falmata.dawano@gmail.com>
2020-11-16 18:20:34 -0600

    revert to createPool

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: "us-cdbr-east-02.cleardb.com",
     user: "bd7768d53cf021",
     password: "96d5b738",
